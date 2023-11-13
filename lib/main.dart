e4d26a01baab8452964bc122ad691b59753c9275
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-11-13 16:18:20 +0900

    初期化

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class start extends StatelessWidget {
+  const start({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router();
+  }
+}
